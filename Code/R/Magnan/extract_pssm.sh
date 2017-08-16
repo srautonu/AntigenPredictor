4799a3996d97502eb83fd4fd274dc6534f06fa55
Saifur Rahman <saifur80@gmail.com>
2017-08-16 16:06:58 +0600

    Extract PSSM script

--- Code/R/Magnan/extract_pssm.sh
+++ Code/R/Magnan/extract_pssm.sh
@@ -1,7 +1,9 @@
-
-for i in *.txt 
+export BLASTDB=$HOME/blastDB
+for i in *.fasta
 do
-    echo $i > gout.txt
-    #echo $i > data${c}.txt
-    psiblast -query $i -db ../../nr/nr -out $i.out -num_iterations 3 -out_ascii_pssm $i.pssm -inclusion_ethresh 0.001 -num_threads 8    
+  echo $i > curProtein.txt
+  echo Running PSI Blast for $i ...
+  psiblast -query $i -db nr -out $i.out -num_iterations 3 -out_ascii_pssm $i.pssm -inclusion_ethresh 0.001 -num_threads 8
+  echo PSI Blast completed for $i.
+  echo $i >> completedProteins.txt
 done
